7a5716755355250ae7d1e159257af5275fc680a7
Jase Pellerin <jasepellerin@gmail.com>
2022-05-09 10:13:49 -0700

    Initialize eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: [
+    // This behaves strangely, removing it for now.
+    '@react-native-community', // Re added because the lint was not working
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react'
+  ],
+  env: {
+    node: true,
+    browser: true,
+    jest: true
+  },
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-expressions': [
+      'error',
+      { allowShortCircuit: true, allowTernary: true }
+    ],
+    '@typescript-eslint/no-use-before-define': 'off',
+    'import/order': 'off',
+    'import/prefer-default-export': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'error',
+    'react/require-default-props': 'off'
+  }
+}
---
+++ prettierrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 100,
+
+  // import order https://github.com/trivago/prettier-plugin-sort-imports
+  // third party modules are automatically moved to the top
+  importOrder: [
+    '^pages/(.*)$',
+    '^partials/(.*)$',
+    '^widgets/(.*)$',
+    '^hooks/(.*)$',
+    '^utils/(.*)$',
+    '^contexts/(.*)$',
+    '^assets/(.*)$',
+    '^types/(.*)$',
+    '^(./)',
+    '^(../)'
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  overrides: [
+    // Overrides for local json
+    {
+      files: ['**/package.json', '**/locales/**/*.json', '.github/**/*.yml'],
+      options: {
+        useTabs: false,
+        tabWidth: 2
+      }
+    }
+  ]
+}
